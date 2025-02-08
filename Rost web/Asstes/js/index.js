b273712003e20eb95fc01b60daaebd19b549cee5
MohamedInsaf <mohamedinsaf8.mi@gmail.com>
2025-02-08 06:41:33 +0530

    Rost

---
+++ Rost web/Asstes/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".carousel").owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: { items: 1, nav: false },
+        600: { items: 2, nav: false },
+        1000: { items: 3, nav: false }
+      },
+    });
+  });
+  
